1f8c6e794e265e3f5df5f7d94c93ac7165b095ba
ravi_kumar <ravi.kumar@hammerspace.com>
2022-01-04 09:02:23 +0000

    HS-13802: 1. Check if share create task is already running, from task list. 2. Fix file indentation. 3. Fix some test case with drives and share create. 4. TODO- TestGetNFSExports is failing 5. TODO- TestShareCreate is failing.

--- pkg/client/hsclient_test.go
+++ pkg/client/hsclient_test.go
@@ -17,163 +17,163 @@ limitations under the License.
 package client
 
 import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "net/http/httptest"
-    "reflect"
-    "testing"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"reflect"
+	"testing"
 
-    //log "github.com/sirupsen/logrus"
+	//log "github.com/sirupsen/logrus"
 
-    common "github.com/hammer-space/csi-plugin/pkg/common"
-    testutils "github.com/hammer-space/csi-plugin/test/utils"
+	common "github.com/hammer-space/csi-plugin/pkg/common"
+	testutils "github.com/hammer-space/csi-plugin/test/utils"
 )
 
 var (
-    Mux      *http.ServeMux
-    Server   *httptest.Server
-    hsclient *HammerspaceClient
+	Mux      *http.ServeMux
+	Server   *httptest.Server
+	hsclient *HammerspaceClient
 )
 
 func setupHTTP() {
-    Mux = http.NewServeMux()
-    Server = httptest.NewServer(Mux)
+	Mux = http.NewServeMux()
+	Server = httptest.NewServer(Mux)
 
-    httpclient := http.DefaultClient
-    hsclient = &HammerspaceClient{
-        username:   "test_user",
-        password:   "test_password",
-        endpoint:   Server.URL,
-        httpclient: httpclient,
-    }
+	httpclient := http.DefaultClient
+	hsclient = &HammerspaceClient{
+		username:   "test_user",
+		password:   "test_password",
+		endpoint:   Server.URL,
+		httpclient: httpclient,
+	}
 }
 
 func tearDownHTTP() {
-    Server.Close()
+	Server.Close()
 }
 
 func TestListShares(t *testing.T) {
-    //log.SetLevel(log.DebugLevel)
-    setupHTTP()
-    defer tearDownHTTP()
+	//log.SetLevel(log.DebugLevel)
+	setupHTTP()
+	defer tearDownHTTP()
 
-    fakeResponse := "[]"
-    fakeResponseCode := 200
+	fakeResponse := "[]"
+	fakeResponseCode := 200
 
-    Mux.HandleFunc(BasePath+"/shares", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, fakeResponse)
-        w.WriteHeader(fakeResponseCode)
-    })
-    shares, err := hsclient.ListShares()
-    if err != nil {
-        t.Error(err)
-    } else if len(shares) != 0 {
-        t.Logf("List shares not empty")
-        t.FailNow()
-    }
+	Mux.HandleFunc(BasePath+"/shares", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, fakeResponse)
+		w.WriteHeader(fakeResponseCode)
+	})
+	shares, err := hsclient.ListShares()
+	if err != nil {
+		t.Error(err)
+	} else if len(shares) != 0 {
+		t.Logf("List shares not empty")
+		t.FailNow()
+	}
 
-    fakeResponse = fmt.Sprintf("[%s,%s]", FakeShareRoot, FakeShare1)
+	fakeResponse = fmt.Sprintf("[%s,%s]", FakeShareRoot, FakeShare1)
 
-    shares, err = hsclient.ListShares()
-    if err != nil {
-        t.Error(err)
-    } else if len(shares) != 2 {
-        t.Logf("Incorrect number of shares")
-        t.FailNow()
-    }
+	shares, err = hsclient.ListShares()
+	if err != nil {
+		t.Error(err)
+	} else if len(shares) != 2 {
+		t.Logf("Incorrect number of shares")
+		t.FailNow()
+	}
 
-    expectedShares := []common.ShareResponse{
-        common.ShareResponse{
-            Name:         "root",
-            ExportPath:   "/",
-            ExtendedInfo: map[string]string{},
-            ShareState:   "PUBLISHED",
-            ExportOptions: []common.ShareExportOptions{
-                common.ShareExportOptions{
-                    Subnet:            "*",
-                    AccessPermissions: "RW",
-                    RootSquash:        false,
-                },
-            },
-            Space: common.ShareSpaceResponse{
-                Total:     "64393052160",
-                Used:      "0",
-                Available: "63909851136",
-            },
-        },
-        common.ShareResponse{
-            Name:       "test-client-code",
-            ExportPath: "/test-client-code",
-            ExtendedInfo: map[string]string{
-                "csi_created_by_plugin_version": "test_version",
-                "csi_created_by_plugin_name":    "test_plugin",
-                "csi_delayed_delete":            "0",
-                "csi_created_by_plugin_git_hash": "",
-                "csi_created_by_csi_version": "1",
-            },
-            Size:       1073741824,
-            ShareState: "PUBLISHED",
-            ExportOptions: []common.ShareExportOptions{
-                common.ShareExportOptions{
-                    Subnet:            "*",
-                    AccessPermissions: "RW",
-                    RootSquash:        false,
-                },
-            },
-            Space: common.ShareSpaceResponse{
-                Total:     "1073741824",
-                Used:      "0",
-                Available: "1073741824",
-            },
-        },
-    }
+	expectedShares := []common.ShareResponse{
+		common.ShareResponse{
+			Name:         "root",
+			ExportPath:   "/",
+			ExtendedInfo: map[string]string{},
+			ShareState:   "PUBLISHED",
+			ExportOptions: []common.ShareExportOptions{
+				common.ShareExportOptions{
+					Subnet:            "*",
+					AccessPermissions: "RW",
+					RootSquash:        false,
+				},
+			},
+			Space: common.ShareSpaceResponse{
+				Total:     "64393052160",
+				Used:      "0",
+				Available: "63909851136",
+			},
+		},
+		common.ShareResponse{
+			Name:       "test-client-code",
+			ExportPath: "/test-client-code",
+			ExtendedInfo: map[string]string{
+				"csi_created_by_plugin_version":  "test_version",
+				"csi_created_by_plugin_name":     "test_plugin",
+				"csi_delayed_delete":             "0",
+				"csi_created_by_plugin_git_hash": "",
+				"csi_created_by_csi_version":     "1",
+			},
+			Size:       1073741824,
+			ShareState: "PUBLISHED",
+			ExportOptions: []common.ShareExportOptions{
+				common.ShareExportOptions{
+					Subnet:            "*",
+					AccessPermissions: "RW",
+					RootSquash:        false,
+				},
+			},
+			Space: common.ShareSpaceResponse{
+				Total:     "1073741824",
+				Used:      "0",
+				Available: "1073741824",
+			},
+		},
+	}
 
-    if !reflect.DeepEqual(shares, expectedShares) {
-        t.Logf("Shares not equal")
-        t.Logf("Expected: %v", expectedShares)
-        t.Logf("Actual: %v", shares)
-        t.FailNow()
-    }
+	if !reflect.DeepEqual(shares, expectedShares) {
+		t.Logf("Shares not equal")
+		t.Logf("Expected: %v", expectedShares)
+		t.Logf("Actual: %v", shares)
+		t.FailNow()
+	}
 
-    fakeResponseCode = 500
-    _, err = hsclient.ListShares()
-    if err != nil {
-        t.Logf("Expected error")
-        t.Fail()
-    }
+	fakeResponseCode = 500
+	_, err = hsclient.ListShares()
+	if err != nil {
+		t.Logf("Expected error")
+		t.Fail()
+	}
 }
 
 func TestCreateShare(t *testing.T) {
-    setupHTTP()
-    defer tearDownHTTP()
+	setupHTTP()
+	defer tearDownHTTP()
 
-    fakeResponseCode := 202
-    expectedCreateShareBody := ""
+	fakeResponseCode := 202
+	expectedCreateShareBody := ""
 
-    // Fake create share
-    Mux.HandleFunc(BasePath+"/shares", func(w http.ResponseWriter, r *http.Request) {
-        w.Header().Set("Location", "http://fake_location/tasks/99184048-9390-4e68-92b8-d3ce6413372d")
-        w.WriteHeader(fakeResponseCode)
-        bodyString, _ := ioutil.ReadAll(r.Body)
-        equal, err := testutils.AreEqualJSON(string(bodyString), expectedCreateShareBody)
-        if err != nil {
-            t.Error(err)
-        }
-        if !equal {
-            t.Fail()
-        }
-    })
+	// Fake create share
+	Mux.HandleFunc(BasePath+"/shares", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Location", "http://fake_location/tasks/99184048-9390-4e68-92b8-d3ce6413372d")
+		w.WriteHeader(fakeResponseCode)
+		bodyString, _ := ioutil.ReadAll(r.Body)
+		equal, err := testutils.AreEqualJSON(string(bodyString), expectedCreateShareBody)
+		if err != nil {
+			t.Error(err)
+		}
+		if !equal {
+			t.Fail()
+		}
+	})
 
-    fakeTaskResponse := fmt.Sprintf("%s", FakeTaskCompleted)
-    fakeTaskResponseCode := 200
-    Mux.HandleFunc(BasePath+"/tasks/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, fakeTaskResponse)
-        w.WriteHeader(fakeTaskResponseCode)
-    })
+	fakeTaskResponse := fmt.Sprintf("%s", FakeTaskCompleted)
+	fakeTaskResponseCode := 200
+	Mux.HandleFunc(BasePath+"/tasks/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, fakeTaskResponse)
+		w.WriteHeader(fakeTaskResponseCode)
+	})
 
-    // test basic
-    expectedCreateShareBody = fmt.Sprintf(`
+	// test basic
+	expectedCreateShareBody = fmt.Sprintf(`
         {"name":"test",
          "path":"/test",
          "extendedInfo":{
@@ -186,40 +186,40 @@ func TestCreateShare(t *testing.T) {
          "shareSizeLimit":0,
          "exportOptions":[]}
     `, common.Version, common.CsiPluginName, common.Githash, common.CsiVersion)
-    err := hsclient.CreateShare("test",
-        "/test", -1,
-        []string{}, []common.ShareExportOptions{}, 0)
-    if err != nil {
-        t.Error(err)
-    }
+	err := hsclient.CreateShare("test",
+		"/test", -1,
+		[]string{}, []common.ShareExportOptions{}, 0, "")
+	if err != nil {
+		t.Error(err)
+	}
 
-    // test multiple objectives
+	// test multiple objectives
 
-    t.Log("Test Multiple Objectives")
-    Mux.HandleFunc(BasePath+"/shares/test/objective-set", func(w http.ResponseWriter, r *http.Request) {
-        w.WriteHeader(200)
-        if r.Method != "POST" {
-            t.Logf("Fail: this should be a POST method")
-            t.Fail()
-        }
-        if !((r.URL.Query()["objective-identifier"][0] == "test-obj") || (r.URL.Query()["objective-identifier"][0] == "test-obj2")) {
-            t.Logf("Fail: Incorrect Objective %s", r.URL.Query()["objective-identifier"][0])
-            t.Fail()
-        }
-    })
+	t.Log("Test Multiple Objectives")
+	Mux.HandleFunc(BasePath+"/shares/test/objective-set", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+		if r.Method != "POST" {
+			t.Logf("Fail: this should be a POST method")
+			t.Fail()
+		}
+		if !((r.URL.Query()["objective-identifier"][0] == "test-obj") || (r.URL.Query()["objective-identifier"][0] == "test-obj2")) {
+			t.Logf("Fail: Incorrect Objective %s", r.URL.Query()["objective-identifier"][0])
+			t.Fail()
+		}
+	})
 
-    err = hsclient.CreateShare("test",
-        "/test",
-        -1, []string{"test-obj", "test-obj2"},
-        []common.ShareExportOptions{},
-        0)
-    if err != nil {
-        t.Error(err)
-    }
+	err = hsclient.CreateShare("test",
+		"/test",
+		-1, []string{"test-obj", "test-obj2"},
+		[]common.ShareExportOptions{},
+		0, "")
+	if err != nil {
+		t.Error(err)
+	}
 
-    // test share size
-    t.Log("Test Share Size")
-    expectedCreateShareBody = fmt.Sprintf(`
+	// test share size
+	t.Log("Test Share Size")
+	expectedCreateShareBody = fmt.Sprintf(`
         {"name":"test",
          "path":"/test",
          "extendedInfo":{
@@ -231,19 +231,19 @@ func TestCreateShare(t *testing.T) {
          "shareSizeLimit":100,
          "exportOptions":[]}
     `, common.Version, common.CsiPluginName, common.Githash, common.CsiVersion)
-    err = hsclient.CreateShare("test",
-        "/test",
-        100,
-        []string{},
-        []common.ShareExportOptions{},
-        -1)
-    if err != nil {
-        t.Error(err)
-    }
+	err = hsclient.CreateShare("test",
+		"/test",
+		100,
+		[]string{},
+		[]common.ShareExportOptions{},
+		-1, "")
+	if err != nil {
+		t.Error(err)
+	}
 
-    // test multiple export options
-    t.Log("Test Multiple export options")
-    expectedCreateShareBody = fmt.Sprintf(`
+	// test multiple export options
+	t.Log("Test Multiple export options")
+	expectedCreateShareBody = fmt.Sprintf(`
         {"name":"test",
          "path":"/test",
          "extendedInfo":{
@@ -267,32 +267,32 @@ func TestCreateShare(t *testing.T) {
             }
          ]}
     `, common.Version, common.CsiPluginName, common.Githash, common.CsiVersion)
-    exportOptions := []common.ShareExportOptions{
-        common.ShareExportOptions{
-            Subnet:            "172.168.0.0/24",
-            AccessPermissions: "RW",
-            RootSquash:        false,
-        },
-        common.ShareExportOptions{
-            Subnet:            "*",
-            AccessPermissions: "RO",
-            RootSquash:        true,
-        },
-    }
-    err = hsclient.CreateShare("test",
-        "/test",
-        100,
-        []string{},
-        exportOptions,
-        0)
-    if err != nil {
-        t.Error(err)
-    }
+	exportOptions := []common.ShareExportOptions{
+		common.ShareExportOptions{
+			Subnet:            "172.168.0.0/24",
+			AccessPermissions: "RW",
+			RootSquash:        false,
+		},
+		common.ShareExportOptions{
+			Subnet:            "*",
+			AccessPermissions: "RO",
+			RootSquash:        true,
+		},
+	}
+	err = hsclient.CreateShare("test",
+		"/test",
+		100,
+		[]string{},
+		exportOptions,
+		0, "")
+	if err != nil {
+		t.Error(err)
+	}
 
-    // test share creation fails on backend
-    t.Log("Test Share Creation Fails")
-    fakeTaskResponse = fmt.Sprintf("%s", FakeTaskFailed)
-    expectedCreateShareBody = fmt.Sprintf(`
+	// test share creation fails on backend
+	t.Log("Test Share Creation Fails")
+	fakeTaskResponse = fmt.Sprintf("%s", FakeTaskFailed)
+	expectedCreateShareBody = fmt.Sprintf(`
         {"name":"test",
          "path":"/test",
          "extendedInfo":{
@@ -305,9 +305,9 @@ func TestCreateShare(t *testing.T) {
          "shareSizeLimit":0,
          "exportOptions":[]}
     `, common.Version, common.CsiPluginName, common.Githash, common.CsiVersion)
-    err = hsclient.CreateShare("test", "/test", -1, []string{}, []common.ShareExportOptions{}, 0)
-    if err == nil {
-        t.Logf("Expected error")
-        t.Fail()
-    }
+	err = hsclient.CreateShare("test", "/test", -1, []string{}, []common.ShareExportOptions{}, 0, "")
+	if err == nil {
+		t.Logf("Expected error")
+		t.Fail()
+	}
 }
